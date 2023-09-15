4cf272d4a4cb144a13d604ea4ff39e02253f5080
Ryota Ikezawa <pavegy@gmail.com>
2023-09-16 06:43:00 +0900

    :recycle: Refactoring install script

--- dots.sh
+++ dots.sh
@@ -3,37 +3,37 @@
 # @see https://wiki.archlinux.jp/index.php/XDG_Base_Directory
 export XDG_CONFIG_HOME=$HOME/.config
 export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship.toml
+export DOTPATH=$HOME/src/github.com/paveg/dots
+export ZDOTPATH="$DOTPATH/zsh.d"
+export ZMODPATH="$ZDOTPATH/mods"
 
-dotpath=$(ghq root)/github.com/paveg/dots
 gitpath=$XDG_CONFIG_HOME/git
+zdotpath=$XDG_CONFIG_HOME/zsh
+nvimpath=$XDG_CONFIG_HOME/nvim
 
+# Loading utils library
 # shellcheck source=/dev/null
 source "$ZMODPATH/util.zsh"
+ln -sf "$DOTPATH/starship.toml" "$STARSHIP_CONFIG"
 
-# zsh
-if [ ! -f "$HOME/.zshenv" ]; then
-	ln -sf "$dotpath/.zshenv" "$HOME/.zshenv"
-	log_pass "Initialized $HOME/.zshenv"
-fi
-if [[ ! -d "$ZDOTDIR" ]]; then
-	mkdir -p "$ZDOTDIR"
-	ln -sf "$dotpath/zsh.d/.zshrc" "$ZDOTDIR/.zshrc"
-	ln -sf "$dotpath/zsh.d/.zprofile" "$ZDOTDIR/.zprofile"
-	ln -sf "$dotpath/zsh.d/.zshenv" "$ZDOTDIR/.zshenv"
-fi
-log_pass "Initialization zsh successfully!"
 
-# starship
-if [[ ! -f $STARSHIP_CONFIG ]]; then
-	ln -sf "$dotpath/starship.toml" "$STARSHIP_CONFIG"
-fi
+[ -d $zdotpath ] && mkdir -p $zdotpath
+zdirs=$(find $ZDOTPATH/.z* -maxdepth 0 -type f)
+ln -snfv "$DOTPATH/.zshenv" "$HOME/.zshenv"
+for f in $zdirs; do
+	fn=$(basename "$f")
+	ln -snfv "$ZDOTPATH/$fn" "$zdotpath/$fn"
+done
+log_info "Completed initializing zshell..."
 
-# git
-if [[ ! -d $gitpath ]]; then
-	mkdir -p "$gitpath"
-	log_pass "Created $gitpath"
-fi
-# shellcheck source=/dev/null
-source ./git/symlink.sh
+
+dirs=$(find $DOTPATH/* -maxdepth 0 -type d)
+for d in $dirs; do
+	name=$(basename "$d")
+	[ $name = "fzf" ] && continue
+	[ $name = "git" ] && ln -snfv "$DOTPATH/git" "$gitpath"
+	[ $name = "nvim" ] && ln -snfv "$DOTPATH/nvim" "$nvimpath"
+done
+log_info "Completed initializing .config..."
 
 log_pass "Completed initializing dots 🎉"
