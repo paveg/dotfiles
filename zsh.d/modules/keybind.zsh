f2f95560d9627f2040218a1c54a9cb53d6c89885
Ryota Ikezawa <pavegy@gmail.com>
2024-06-30 04:44:10 +0900

    chore(zeno): Install

--- zsh.d/modules/keybind.zsh
+++ zsh.d/modules/keybind.zsh
@@ -3,3 +3,19 @@ bindkey -e
 # _fzf_cd_ghq / ^g
 zle -N _fzf_cd_ghq
 bindkey "^[" _fzf_cd_ghq
+
+if [[ -n $ZENO_LOADED ]]; then
+  echo "zeno loaded"
+  bindkey ' '  zeno-auto-snippet
+
+  bindkey '^m' zeno-auto-snippet-and-accept-line
+  bindkey '^i' zeno-completion
+  bindkey '^x '  zeno-insert-space
+  bindkey '^x^m' accept-line
+  bindkey '^x^z' zeno-toggle-auto-snippet
+
+  # bindkey '^r'   zeno-history-selection
+  bindkey '^x^s' zeno-insert-snippet
+  bindkey '^x^f' zeno-ghq-cd
+fi
+
